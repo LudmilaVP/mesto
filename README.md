7cd8d35e76c0ea0c2e60f40c8cc5fc000daaa9ae
Liudmila Lupandina <liudmila.lupandina@yandex.ru>
2022-05-19 00:03:17 +0300

    refactor:реализовать readme

---
+++ README.md
@@ -0,0 +1,25 @@
+# «Место» - проектная работа № 4 для Яндекс.Практикум
+
+## О проекте
+
+Сайт, на котором пользователь может редактировать интерактивную форму, вводить любое имя и профессию.
+
+## Стек
+
+* HTML
+* CSS
+* Javascript
+
+## Реализация
+
+* Grid Layout
+* Flexbox-вёрстка
+* Медиазапросы
+* Адаптивная верстка
+* БЭМ Nested
+* Функционал на JavaScript
+
+## Ссылки на проект и на макет:
+
+* [Проект на Github Pages](https://ludmilavp.github.io/mesto/)
+* [Макет в Figma](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+const editButton = document.querySelector('.profile__edit-button');
+const closeButton = document.querySelector('.popup__close');
+let formElement = document.querySelector('.popup');
+let usernameInput = document.querySelector('.popup__input_type_username');
+let jobInput = document.querySelector('.popup__input_type_job');
+let username = document.querySelector('.profile__title');
+let job = document.querySelector('.profile__description');
+let form = document.querySelector('.popup__form');
+
+function openPopup() {
+    formElement.classList.add('popup_opened');
+    usernameInput.value = username.textContent;
+    jobInput.value = job.textContent;
+}
+
+function closePopup() {
+    formElement.classList.remove('popup_opened');
+}
+
+function savePopup(evt) {
+    evt.preventDefault();
+    username.textContent = usernameInput.value;
+    job.textContent = jobInput.value;
+    closePopup();
+}
+
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
+form.addEventListener('submit', savePopup);
